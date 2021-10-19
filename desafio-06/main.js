b29a1c16e1e93c554d6f3d62f23f20881efc44d7
Felipe Xavier <felipe_rsr@live.com>
2021-10-19 16:42:31 -0300

    update

--- desafio-06/main.js
+++ desafio-06/main.js
@@ -1,8 +1,31 @@
-const btnMobile = document.querySelector("#btn-mobile")
-
-function toggleMenu(params) {
-  const nav = document.querySelector("#nav")
-  nav.classList.toggle('ativo')
+// Função menu mobile
+function iniMenuMobile() {
+  const btnMobile = document.querySelector("#btn-mobile")
+  
+  function toggleMenu(params) {
+    const nav = document.querySelector("#nav")
+    nav.classList.toggle('ativo')
+  }
+  
+  btnMobile.addEventListener('click', toggleMenu)
 }
 
-btnMobile.addEventListener('click', toggleMenu)
+// Função accordion list
+function initAccosdionList() {
+  const dt = document.querySelectorAll('dt')
+  
+  
+  dt.forEach((item) =>{
+    
+    item.addEventListener('click', function () {
+      
+      
+      this.classList.toggle('ativo')
+      item.nextElementSibling.classList.toggle('ativo')
+    })
+  })
+  
+}
+
+initAccosdionList()
+iniMenuMobile()
